b2c9d91b6cc083d335b9473cb0b859f3c192fbb6
Simon Wilson <sjw@softwire.com>
2015-12-22 22:10:53 +0530

    Adding static homepage and moving all the spring stuff to /dynamic (for reasons which will become clear later)

--- src/main/java/whatnow/controller/HomeController.java
+++ src/main/java/whatnow/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("/dynamic")
     public ModelAndView helloWorld() {
         return new ModelAndView("home");
     }
